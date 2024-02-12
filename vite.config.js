10b0111ef3c8caf93fa6c4ce113e39d75b5d58bb
Paul <cpauljmnz22gmail.com>
2024-02-11 21:39:17 -0600

    base vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://cpauljmnz.github.io/test-cuentas",
   build: {
     chunkSizeWarningLimit: 1000000,
   },
