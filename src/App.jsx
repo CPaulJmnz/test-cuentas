2fce509645d6a98fe22e2dd0f28d50a286acbe24
Paul <cpauljmnz22gmail.com>
2024-02-14 21:47:44 -0600

    responsive movil

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,23 @@ function App() {
   return (
     <GlobalProvider>
       <div className='bg-zinc-900 text-white min-h-screen flex justify-center items-center'>
-        <div className='container mx-auto w-auto'>
-          <div className='bg-zinc-800 p-10 rounded-lg flex gap-x-2'>
-            <div>
+        <div className='lg:bg-zinc-800 container mx-auto w-full lg:w-3/4 xl:flex lg:rounded-lg'>
+          <div className='bg-zinc-800 lg:w-full p-4 lg:p-10 rounded-lg flex flex-col lg:flex-row lg:gap-4 mt-4'>
+            <div className='w-full lg:w-2/3'>
               <IncomeExpenses/>
               <Balance/>
               <TransactionForm/>
             </div>
-            <div>
+            <div className='w-full'>
               <ExpenseChart/>
             </div>
-            <div className='w-64'>
-              <TransactionList/>
-            </div>
+          </div>
+          <div className='bg-zinc-800 p-4 mt-4 rounded-lg xl:w-1/2'>
+            <TransactionList/>
           </div>
         </div>
       </div>
     </GlobalProvider>
-    
   )
 }
 
